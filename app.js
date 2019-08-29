88d4560ef3db78e5ec1856f76f08e32dec65ff3d
Lucas Salmons <lucsalmons@gmail.com>
2019-08-28 23:06:15 -0700

    Express Session Part 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+
 var logger = require('morgan');
 
 var session = require('express-session');
@@ -42,35 +42,20 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 //authorization
 function auth(req, res, next) {
+  console.log(req.session);
+
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
-    //encripted header after space converted to string and split to user/pass
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-      res.session.user = 'admin';//declare user on first session
-      next(); //authorized success
-    } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
   }
   else {
-    //check if existing signed cookie is correct
-    if (req.session.user === 'admin') {
-      console.log('req.session: ', req.session);
+    if (req.session.user === 'authenticated') {
       next();
     }
     else {
@@ -80,11 +65,10 @@ function auth(req, res, next) {
     }
   }
 }
+
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
