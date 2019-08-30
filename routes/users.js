6dfbf19f215e50d0f2d7cb07b4f7b63d1aaefee7
Lucas Salmons <lucsalmons@gmail.com>
2019-08-30 00:49:05 -0700

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,19 @@ var User = require('../models/user');
 var passport = require('passport');
 var router = express.Router();
 router.use(bodyParser.json());
+var authenticate = require('../authenticate');
 
 /* GET users listing. */
 //implement passport
 router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signup', (req, res, next) => {
   User.register(new User({ username: req.body.username }),
     req.body.password, (err, user) => {
       if (err) {
-      res.statusCode = 500;
+        res.statusCode = 500;
         res.setHeader('Content-Type', 'application/json');
         res.json({ err: err });
       }
@@ -26,9 +31,11 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+
+  var token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({ success: true, status: 'You are successfully logged in!' });
+  res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
 router.get('/logout', (req, res) => {
