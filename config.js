388c053eb059cd7aa6a874498c78325d212b0f7c
Lucas Salmons <lucsalmons@gmail.com>
2019-09-04 22:17:42 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2513263722228721',
+        clientSecret: 'd75824763c6173a68cfbc83084cb48a9'
+    }
 }
